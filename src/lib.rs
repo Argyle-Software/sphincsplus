e496424c329e10674abede693da073fe2e4dd43b
Mitchell Berry <foss@mitchellberry.com>
2022-07-07 11:51:04 +0800

    moved hash and thash types into their own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,27 @@
-
+#![feature(generic_const_exprs)]
 
 mod address;
 mod context;
+mod fips202;
 mod fors;
 mod haraka;
 mod haraka_offsets;
-mod hash_haraka;
+mod hash;
+// mod hash_haraka;
 // mod hash_sha2;
 // mod hash_shake;
 mod merkle;
 mod params;
 // mod sha2;
 mod sign;
-// mod thash_haraka_robust;
-mod thash_haraka_simple;
-// mod thash_sha2_robust;
-// mod thash_sha2_simple;
-// mod thash_shake_robust;
-// mod thash_shake_simple;
+mod thash;
 mod utils;
 mod utilsx1;
 mod wots;
 mod wotsx1;
 mod randombytes;
 
-pub use params::*;
-pub use sign::*;
+pub use params::{
+  CRYPTO_BYTES, CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES, CRYPTO_SEEDBYTES
+};
+pub use sign::{crypto_sign_keypair, crypto_sign, crypto_sign_open};
