8722ddf887afaf0c8e908e21442a897e284c31f6
Mitchell Berry <foss@mitchellberry.com>
2022-09-13 15:50:52 +0800

    Add docs readme use public api

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+//! # SPHINCS<sup>+</sup>
+//!
+//! A rust implementation of the SPHINCS<sup>+</sup> stateless hash-based signature scheme, 
+//! which has been included in NIST's post-quantum cryptographic standard.
+//! 
+//! It is highly recommended to use SPHINCS<sup>+</sup> in a hybrid system alongside a 
+//! traditional signature algorithm such as RSA or ed25519. 
+//! 
+//! ---
+//! 
+//! ## Usage 
+//! ```
+//!  let keys = keypair();
+//!  let msg = [0u8; 32];
+//!  let sig = sign(&msg, &keys);
+//!  let sig_verify = verify(&sig, &msg, &keys);
+//!  assert(sig_verify.is_ok());
+//! ```
+//! To compile this library needs one from each of the following categories to be 
+//! enabled, using more than one from each group will result in a compile error. 
+//! 
+//! The security levels target 128, 192 and 256 bit equivalents, corresponding to NIST
+//! levels 1,3,5 respectively. They are also separated into **fast** (f) and **small** 
+//! (s) subtypes, which make the tradeoff between either quicker signing or smaller 
+//! signatures sizes.
+//! 
+//! 
+//! * ### Hash
+//!   * `haraka`
+//!   * `sha2`
+//!   * `shake`
+//! 
+//! * ### Security Level
+//!   * `f128`
+//!   * `f192`
+//!   * `f256`
+//!   * `s128`
+//!   * `s192`
+//!   * `s256`
+//! * ### TreeHash
+//!   * `simple`
+//!   * `robust`
+//! 
+//! For example: 
+//! 
+//! ```toml
+//! [dependencies]
+//! pqc_sphincsplus = {version = "0.1.0", features = ["haraka", "f128", "simple"]}
+//! ```
+//! 
+//! A comparison of the different security levels is below.
+//! 
+//! 
+//! |               | n  | h  | d  | log(t) | k  |  w  | bit security | pk bytes | sk bytes | sig bytes |
+//! | :------------ | -: | -: | -: | -----: | -: | --: | -----------: | -------: | -------: | --------: |
+//! | SPHINCS+-128s | 16 | 63 |  7 |     12 | 14 |  16 |          133 |       32 |       64 |     7,856 |
+//! | SPHINCS+-128f | 16 | 66 | 22 |      6 | 33 |  16 |          128 |       32 |       64 |    17,088 |
+//! | SPHINCS+-192s | 24 | 63 |  7 |     14 | 17 |  16 |          193 |       48 |       96 |    16,224 |
+//! | SPHINCS+-192f | 24 | 66 | 22 |      8 | 33 |  16 |          194 |       48 |       96 |    35,664 |
+//! | SPHINCS+-256s | 32 | 64 |  8 |     14 | 22 |  16 |          255 |       64 |      128 |    29,792 |
+//! | SPHINCS+-256f | 32 | 68 | 17 |      9 | 35 |  16 |          255 |       64 |      128 |    49,856 |
+//! 
+
+
 #![no_std]
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
@@ -11,6 +75,7 @@
 )))] 
 compile_error!("Must choose one from each category of the feature set to build");
 
+mod api;
 mod address;
 mod context;
 mod fors;
@@ -32,11 +97,7 @@ mod sha2;
 #[cfg(feature = "haraka")] 
 mod haraka;
 
-pub use sign::{
-  crypto_sign_keypair, 
-  crypto_sign, 
-  crypto_sign_open
-};
+pub use api::*;
 
 pub use params::{
   CRYPTO_BYTES, 
