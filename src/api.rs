47ae4081970266552232cff6ffee6d58d6c06a0c
Mitchell Berry <foss@mitchellberry.com>
2022-09-13 15:50:22 +0800

    add public api

---
+++ src/api.rs
@@ -0,0 +1,59 @@
+use crate::params::{CRYPTO_SECRETKEYBYTES, CRYPTO_PUBLICKEYBYTES, SPX_BYTES};
+use crate::sign::*;
+
+#[derive(Copy, Clone)]
+pub struct Keypair {
+  public: [u8; CRYPTO_PUBLICKEYBYTES],
+  secret: [u8; CRYPTO_SECRETKEYBYTES]
+}
+
+pub enum SigError {
+  Input,
+  Verify
+}
+
+/// Generates a keypair for signing and verification
+/// 
+/// Example: 
+/// ```no_run
+/// let keys = keypair();
+/// assert!(keys.public.len() == CRYPTO_PUBLICKEYBYTES);
+/// assert!(keys.secret.len() == CRYPTO_SECRETKEYBYTES);
+/// ```
+pub fn keypair() -> Keypair
+{
+  let mut public = [0u8; CRYPTO_PUBLICKEYBYTES];
+  let mut secret = [0u8; CRYPTO_SECRETKEYBYTES];
+  crypto_sign_keypair(&mut public, &mut secret, None);
+  Keypair { public, secret }
+}
+
+/// Generates a signature for the given message using a keypair
+/// 
+/// Example: 
+/// ```no_run
+/// # let keys = keypair();
+/// let msg = [0u8; 32];
+/// let sig = sign(&msg, &keys);
+/// assert!(sig.len() == SPX_BYTES);
+/// ```  
+pub fn sign(msg: &[u8], keypair: &Keypair) -> [u8; SPX_BYTES] 
+{
+  let mut sig = [0u8; SPX_BYTES];
+  crypto_sign_signature(&mut sig, msg, &keypair.secret, None);
+  sig
+}
+
+/// Verify signature using keypair
+/// 
+/// Example: 
+/// ```no_run
+/// # let keys = keypair();
+/// # let msg = [0u8; 32];
+/// # let sig = sign(&msg, &keys);
+/// let sig_verify = verify(&sig, &msg, &keys);
+/// assert(sig_verify.is_ok());
+pub fn verify(sig: &[u8], msg: &[u8], keypair: &Keypair) -> Result<(), SigError>
+{
+  crypto_sign_verify(&sig, &msg, &keypair.public)
+}
